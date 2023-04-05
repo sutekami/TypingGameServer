552ea004bd77872a2b82e72db4155dde0d2f5366
mashu <r202103318wt@jindai.jp>
2023-04-05 13:00:24 +0900

    first commit

---
+++ randamStr.js
@@ -0,0 +1,32 @@
+const randomString = [
+    {
+        yomi: 'この緑茶は美味しい',
+        romaji: 'konoryokutyahaoisii'
+    },
+    {
+        yomi: '天真爛漫な子だ',
+        romaji: 'tennsinnrannmannnakoda'
+    },
+    {
+        yomi: 'わたしを離さないで',
+        romaji: 'watasiwohanasanaide'
+    },
+    {
+        yomi: '基本情報技術者',
+        romaji: 'kihonnjouhougijutusya'
+    },
+    {
+        yomi: '今夜は少し寒くなるようだ',
+        romaji: 'konnyahasukosisamukunaruyouda'
+    }
+]
+
+exports.randamStr = function() {
+    let randomStringArray = [];
+    for (let i = 0; i < 3; i++){
+        const randomStringLength = Math.trunc(Math.random() * randomString.length);
+        randomStringArray.push(randomString[randomStringLength]);
+    }
+
+    return randomStringArray;
+};
---
+++ serve.js
@@ -0,0 +1,122 @@
+const ip = require('ip');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+const PORT = 3001;
+const randamStr = require('./randamStr').randamStr;
+
+function generateUuid() {
+    let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+    for (let i = 0, len = chars.length; i < len; i++) {
+        switch (chars[i]) {
+            case "x":
+                chars[i] = Math.floor(Math.random() * 16).toString(16);
+                break;
+            case "y":
+                chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+                break;
+        }
+    }
+    return chars.join("");
+}
+
+let room_info = [];
+
+io.on('connection', socket => {
+    io.to(socket.id).emit('socket_id', socket.id);
+
+    socket.on('matching', data => {
+        if (room_info.length !== 0){
+            for (let i = 0; i < room_info.length; i++){
+                if (!room_info[i].matching){
+                    socket.join(room_info[i].roomId);
+                    room_info[i].matching = true;
+                    room_info[i].user2 = {
+                        token: data.token,
+                        userName: data.userName,
+                    };
+                    io.to(data.token).emit('room_id', room_info[i].roomId);
+                    io.to(room_info[i].roomId).emit('match', {
+                        room_info: room_info[i],
+                        typingString: randamStr(),
+                    });
+                    console.log(randamStr());
+                    console.log(room_info[i]);
+                } else if (i + 1 === room_info.length){
+                    const roomId = generateUuid();
+                    socket.join(roomId);
+                    room_info.push({
+                        roomId: roomId,
+                        user1: {
+                            token: data.token,
+                            userName: data.userName,
+                        },
+                        user2: null,
+                        matching: false,
+                    });
+                    io.to(data.token).emit('room_id', roomId);
+                    break;
+                }
+            }
+        } else {
+            const roomId = generateUuid();
+            socket.join(roomId);
+            room_info.push({
+                roomId: roomId,
+                user1: {
+                    token: data.token,
+                    userName: data.userName,
+                },
+                user2: null,
+                matching: false,
+            });
+            io.to(data.token).emit('room_id', roomId);
+        }
+    })
+
+    socket.on('beColored', data => {
+        const roomId = data.roomId;
+        for (let i of room_info) {
+            if (roomId === i.roomId){
+                (i.user1.token === data.token) ? io.to(i.user2.token).emit('coloring')
+                : io.to(i.user1.token).emit('coloring');
+                break;
+            }
+        }
+    });
+
+    socket.on('changePartnerString', data => {
+        const roomId = data.roomId;
+        for (let i of room_info){
+            if (roomId === i.roomId){
+                (i.user1.token === data.token) ? io.to(i.user2.token).emit('changeString')
+                : io.to(i.user1.token).emit('changeString');
+                break;
+            }
+        }
+    });
+
+    socket.on('backHome', data => {
+        const roomId = data.roomId;
+        for (let i of room_info){
+            if (roomId === i.roomId){
+                (i.user1.token === data.token) ? io.to(i.user2.token).emit('backHome')
+                : io.to(i.user1.token).emit('backHome');
+                break;
+            }
+        }
+    })
+
+    socket.on('disconnect', () => {
+    });
+})
+
+http.listen(PORT, ip.address(), () => {
+    console.log(`Example app listening on port 3001, ${ip.address()}`);
+})
